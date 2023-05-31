f1335344750f8ea7dba9ebf9855d8ed9c29d2b06
HyewonChoi <hyewonwk2846@sookmyung.ac.kr>
2023-05-31 14:57:02 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/episode/epilepseewatch/MainActivity.java
@@ -0,0 +1,23 @@
+package kr.co.episode.epilepseewatch;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import kr.co.episode.epilepseewatch.databinding.ActivityMainBinding;
+
+public class MainActivity extends Activity {
+
+    private TextView mTextView;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        mTextView = binding.text;
+    }
+}
