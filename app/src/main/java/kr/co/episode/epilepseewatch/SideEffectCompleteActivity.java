b3988d5829bc0c1c321ffc5272954929cc28b0ff
HyewonChoi <hyewonwk2846@sookmyung.ac.kr>
2023-06-09 17:42:43 +0900

    SideEffectCompleteActivity.java 생성

---
+++ app/src/main/java/kr/co/episode/epilepseewatch/SideEffectCompleteActivity.java
@@ -0,0 +1,23 @@
+package kr.co.episode.epilepseewatch;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import kr.co.episode.epilepseewatch.databinding.ActivitySideEffectCompleteBinding;
+
+public class SideEffectCompleteActivity extends Activity {
+
+    private TextView mTextView;
+    private ActivitySideEffectCompleteBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivitySideEffectCompleteBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        mTextView = binding.text;
+    }
+}
